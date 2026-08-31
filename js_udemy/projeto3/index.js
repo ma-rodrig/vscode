7c62edde0558651c02e69066bd1f1b1a122a0844
ovopodre09 <160166395+ovopodre09@users.noreply.github.com>
2026-08-31 23:52:00 +0100

    projeto3

---
+++ js_udemy/projeto3/index.js
@@ -0,0 +1,14 @@
+
+const colors = ['Black', 'Red', 'Blue', 'Yellow', 'Purple', 'Pink']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+	const randomColor = getRandomColor()
+	document.body.style.background = colors[randomColor]
+	color.textContent = colors[randomColor]
+})
+
+function getRandomColor() {
+	return Math.floor(Math.random() * colors.length)
+}
